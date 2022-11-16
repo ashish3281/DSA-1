aff44770a17726bf653eee1ec1912ad855058cd7
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-11-16 07:51:56 +0530

    Add files via upload

---
+++ question8.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+              int n=scn.nextInt();
+        int row=1;
+        int nsp=n-1;
+        int nst=1;
+        while(row<=n){
+            int csp=1;
+            while(csp<=nsp){
+            System.out.print("\t");
+            csp+=1;
+        }
+        int cst=1;
+        while(cst<=nst){
+            System.out.print("*\t");
+            cst+=1;
+        }
+        row+=1;
+        nst=1;
+        nsp-=1;
+        System.out.println();
+        
+    }
+
+    }
+}
