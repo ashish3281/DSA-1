2961dba336c34e84422846a79703791b8e9a7caf
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-11-19 15:50:00 +0530

    Add files via upload

---
+++ question30.java
@@ -0,0 +1,16 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  int count=0;
+  while(n!=0){
+      int lastdigit=n%10;
+      count++;
+      n=n/10;
+  }
+  System.out.println(count);
+   }
+  }
