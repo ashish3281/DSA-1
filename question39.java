5a065335974e6bfffaa968ee64babe24ec5cfe5c
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-11-21 21:21:18 +0530

    Add files via upload

---
+++ question39.java
@@ -0,0 +1,15 @@
+public import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("*****");
+    System.out.println("   *");
+    System.out.println("  *");
+    System.out.println(" *");
+    System.out.println("*****");
+
+  }
+} {
+    
+}
