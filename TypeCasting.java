87d173ef821a2c822fd7f81df55860f387792bb2
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-07 19:06:42 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,15 @@
+
+public class TypeCasting {
+    public static void main(String args[]){
+        //wideing type casting
+        int myInt=9;
+        double myDouble=myInt;
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        //narrow type casting
+        double x=9.78d;
+        int y=(int)x;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
