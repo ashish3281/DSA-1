0f6901ff76f3c28afb405cb88d9ccd6c7bb57047
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-07 19:06:19 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,19 @@
+public class variable {
+    public static void main(String args[]){
+        String name="ashish";
+        int rollNum=206010;
+        float a=2.44f;
+        char x='A';
+        boolean mybool=true;
+        System.out.println(name);
+        System.out.println(rollNum);
+        System.out.println(a);
+        System.out.println(x);
+        System.out.println(mybool);
+
+        int z;
+        z=20;
+        System.out.println(z);
+
+    }
+}
