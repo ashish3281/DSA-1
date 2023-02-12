bdf3474fa7cc48ffd357880e7af4ef7b48b80bc2
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-02-12 16:22:30 +0530

    Add files via upload

---
+++ question73.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        int x=scn.nextInt();
+        int n=scn.nextInt();
+        int ans=power(x,n);
+        System.out.println(ans);
+    }
+
+    public static int power(int x, int n){
+        if(n==0){
+            return 1;
+        }
+        int xrnb2=power(x,n/2);
+        int xrn=xrnb2*xrnb2;
+        if(n%2==1){
+            xrn=xrn*x;
+        }
+        return xrn;
+    }
+
+}
