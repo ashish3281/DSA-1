22d4105bed11ee78f35e2d5c1eae649b09c2b2af
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-02-26 19:52:47 +0530

    Add files via upload
    a very big sum

---
+++ practice/question4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            
+        }
+        long sum=0;
+       for(int i=0;i<n;i++){
+         sum=sum+arr[i];
+     }   
+     System.out.println(sum);
+    }
+}
