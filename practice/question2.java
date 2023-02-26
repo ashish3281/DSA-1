40c83482633d807dac932ea9be940bd64765150d
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-02-26 19:51:59 +0530

    Add files via upload
    Simple array sum

---
+++ practice/question2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}
